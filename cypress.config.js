04fbc2879bf3e2ea64820bd16528ae911f394388
gabycavalli <gabrielcavalli@gmail.com>
2025-10-16 12:18:12 -0300

    fix-intercept

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://parabank.parasoft.com/parabank/index.htm",
+    //baseUrl: "https://parabank.parasoft.com/parabank/index.htm",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
